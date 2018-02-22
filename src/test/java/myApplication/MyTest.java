74f9f68434fb82b44cfe6dd8ce34eb4f3d048988
earmarl <marlen.arias@ericsson.com>
2018-02-21 22:02:59 -0600

    Commit pom.xml

--- src/test/java/myApplication/MyTest.java
+++ src/test/java/myApplication/MyTest.java
@@ -13,6 +13,6 @@ public class MyTest {
         // assert statements
         assertTrue("10 x 0 must be 0", tester.multiply(10, 0) == 0);
         assertTrue("0 x 10 must be 0", tester.multiply(0, 10) == 0);
-        assertTrue("0 x 0 must be 0", tester.multiply(0, 0) == 0);
+        assertTrue("0 x 0 must be 0", tester.multiply(0, 0) == 1);
     }
 }
